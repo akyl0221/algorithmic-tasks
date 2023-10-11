cdd4d1902d749ce55486689dcfcb54ae1527cc15
akyldjumaliev <kel.199821@gmail.com>
2023-10-11 21:34:52 +0600

    Solved rectangle

---
+++ leetcode/easy/calculate_rectangle.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        l=[area,1]
+        d=l[0]-l[1]
+        i=2
+        x=0
+        while i*i<=area:
+            if area%i==0:
+                x=area//i
+                if x-i<=d:
+                    l=[x,i]
+            i+=1
+        return l
